d48dead079e4b6480cffab22258bbfa9be7ce295
Saúl Alonso <saul@MacBook-Air-de-Saul.local>
2023-08-27 11:40:20 +0200

    script

--- update_list.py
+++ update_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 CHANNEL_PATTERN = 'http://127.0.0.1:6878/ace/getstream?id='
+CHANNELS_FILTER = ['DAZN', 'Liga', 'LigaTv', 'LaLiga', 'F1', 'M+Deportes']
 
 try:
     src_file = open('src/channels.txt', 'r')
@@ -8,9 +9,12 @@ try:
     dest_lines = ['#EXTM3U\r\n']
 
     for src_line in src_file.readlines():
-        if CHANNEL_PATTERN in src_line:
+        if CHANNEL_PATTERN in src_line and any(word in tmp_line for word in CHANNELS_FILTER):
             dest_lines.append(tmp_line)
-            dest_lines.append(src_line.replace(CHANNEL_PATTERN, ''))
+            if CHANNEL_PATTERN + 'acestream://' in src_line:
+                dest_lines.append(src_line.replace(CHANNEL_PATTERN + 'acestream://', ''))
+            else:
+                dest_lines.append(src_line.replace(CHANNEL_PATTERN, ''))
         tmp_line = src_line
 
     src_file.close()
