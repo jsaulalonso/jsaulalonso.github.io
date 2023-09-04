a8d8646812aa2a9da7dfae0c2ef8e8592fd575a6
Saúl Alonso <saulsb12@gmail.com>
2023-09-04 20:29:15 +0200

    Update update_list.py

--- update_list.py
+++ update_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 CHANNEL_PATTERN = 'http://127.0.0.1:6878/ace/getstream?id='
-CHANNELS_FILTER = ['DAZN F1', 'M+ Liga', 'LaLiga TV', 'M+ Deportes', 'DAZN 1 HD', 'DAZN 2 HD', 'DAZN 3 HD', 'DAZN 4 HD', 'DAZN LaLiga HD', 'DAZN LaLiga 2 HD', 'DAZN LaLiga 3 HD', 'DAZN LaLiga 4 HD', 'DAZN LaLiga 5 HD']
-
+CHANNELS_FILTER = ['DAZN F1', 'M+ Liga', 'LaLiga TV', 'M+ Deportes', 'DAZN 1 HD', 'DAZN 2 HD', 'DAZN 3 HD', 'DAZN 4 HD', 'DAZN LaLiga']
 try:
     src_file = open('src/channels.txt', 'r')
 
